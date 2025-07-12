c1a6fedd84913ef2faedba9f153c9fd8092e6d13
Ravindra Zanjare <zanjareravindra@gmail.com>
2025-07-12 15:17:57 +0200

    Delete src/main directory

--- src/main/java/com/dex/dao/LoginDao.java
+++
@@ -1,8 +0,0 @@
-package com.dex.dao;
-
-import com.dex.models.UserBean;
-
-public interface LoginDao
-{
-    UserBean authenticateUser(String loginId, String password);
-}
--- src/main/java/com/dex/service/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package com.dex.service;
-
-public interface PaymentService {
-	String generatePaymentUrl();
-}
